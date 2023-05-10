dde72ae0939c08ddedccf43756cfcf5701760402
Enes Ay <jcooligame@gmail.com>
2023-05-10 17:21:39 +0300

    started to design pages, create components

--- app/src/main/java/com/nsicyber/deezerpractice/ui/AlbumDetailFragment.kt
+++ app/src/main/java/com/nsicyber/deezerpractice/ui/AlbumDetailFragment.kt
@@ -6,6 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ace1ofspades.recyclerview.GroupAdapter
+import com.ace1ofspades.recyclerview.viewHolders.ViewHolder
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -13,8 +17,10 @@ import com.bumptech.glide.request.RequestOptions
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.nsicyber.deezerpractice.R
+import com.nsicyber.deezerpractice.components.MusicComponent
 import com.nsicyber.deezerpractice.databinding.ActivityMainBinding
 import com.nsicyber.deezerpractice.models.AlbumModel
+import com.nsicyber.deezerpractice.models.MusicModel
 import com.nsicyber.deezerpractice.utils.Parser
 import com.nsicyber.deezerpractice.utils.loadUrlRadius
 
@@ -27,7 +33,11 @@ import com.nsicyber.deezerpractice.utils.loadUrlRadius
 class AlbumDetailFragment : Fragment() {
     private lateinit var collapsing_toolbar_layout: CollapsingToolbarLayout
     private lateinit var image_view: ImageView
+    private lateinit var recyclerView: RecyclerView
+    var adapter = GroupAdapter<ViewHolder>()
+
     var model: AlbumModel? = AlbumModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -40,6 +50,8 @@ class AlbumDetailFragment : Fragment() {
 
         collapsing_toolbar_layout = view.findViewById(R.id.collapsing_toolbar_layout)
         image_view = view.findViewById(R.id.imageView)
+        recyclerView = view.findViewById(R.id.recyclerView)
+
         arguments?.let {
             model = Parser.parse(it.getSerializable("data"))
         }
@@ -56,6 +68,15 @@ class AlbumDetailFragment : Fragment() {
         // Load the image into the image view using Glide
         Glide.with(this).load(model?.cover).placeholder(R.drawable.music_logo)
             .error(R.drawable.music_logo).into(image_view)
+
+
+        recyclerView.layoutManager =
+            LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        recyclerView.adapter = adapter
+        for(i in model?.tracks as ArrayList<MusicModel>)
+        {
+            adapter.add(MusicComponent(i))
+        }
 
 
     }
--- app/src/main/java/com/nsicyber/deezerpractice/ui/ArtistDetailFragment.kt
+++ app/src/main/java/com/nsicyber/deezerpractice/ui/ArtistDetailFragment.kt
@@ -5,13 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ace1ofspades.recyclerview.GroupAdapter
+import com.ace1ofspades.recyclerview.scrollListeners.ItemOffsetDecoration
+import com.ace1ofspades.recyclerview.viewHolders.ViewHolder
 import com.bumptech.glide.Glide
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.nsicyber.deezerpractice.R
+import com.nsicyber.deezerpractice.components.AlbumComponent
+import com.nsicyber.deezerpractice.components.MusicComponent
 import com.nsicyber.deezerpractice.models.AlbumModel
+import com.nsicyber.deezerpractice.models.ArrayAlbumModel
 import com.nsicyber.deezerpractice.models.ArtistModel
+import com.nsicyber.deezerpractice.models.MusicModel
+import com.nsicyber.deezerpractice.network.RetrofitCallback
+import com.nsicyber.deezerpractice.network.RetrofitClient
 import com.nsicyber.deezerpractice.utils.Parser
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * A simple [Fragment] subclass.
@@ -19,40 +35,98 @@ import com.nsicyber.deezerpractice.utils.Parser
  * create an instance of this fragment.
  */
 
-    class ArtistDetailFragment : Fragment() {
-        private lateinit var collapsing_toolbar_layout: CollapsingToolbarLayout
-        private lateinit var image_view: ImageView
-        var model: ArtistModel? = ArtistModel()
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
+class ArtistDetailFragment : Fragment() {
+    private lateinit var collapsing_toolbar_layout: CollapsingToolbarLayout
+    private lateinit var image_view: ImageView
 
-        }
+    private lateinit var recyclerView: RecyclerView
+    var adapter = GroupAdapter<ViewHolder>()
 
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-        ): View? {
-            var view = inflater.inflate(R.layout.fragment_artist_detail, container, false)
-
-            collapsing_toolbar_layout = view.findViewById(R.id.collapsing_toolbar_layout)
-            image_view = view.findViewById(R.id.imageView)
-            arguments?.let {
-                model = Parser.parse(it.getSerializable("data"))
-            }
-            return view
-
-        }
-
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
-
-            // Set the title of the collapsing toolbar
-            collapsing_toolbar_layout.title = model?.name
-
-            // Load the image into the image view using Glide
-            Glide.with(this).load(model?.picture).placeholder(R.drawable.music_logo)
-                .error(R.drawable.music_logo).into(image_view)
-
-
-        }
+    var model: ArtistModel? = ArtistModel()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
     }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        var view = inflater.inflate(R.layout.fragment_artist_detail, container, false)
+
+        collapsing_toolbar_layout = view.findViewById(R.id.collapsing_toolbar_layout)
+        image_view = view.findViewById(R.id.imageView)
+        recyclerView = view.findViewById(R.id.recyclerView)
+
+        arguments?.let {
+            model = Parser.parse(it.getSerializable("data"))
+        }
+        return view
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set the title of the collapsing toolbar
+        collapsing_toolbar_layout.title = model?.name
+
+        // Load the image into the image view using Glide
+        Glide.with(this).load(model?.picture).placeholder(R.drawable.music_logo)
+            .error(R.drawable.music_logo).into(image_view)
+
+        recyclerView.layoutManager =
+            GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
+        recyclerView.adapter = adapter
+        recyclerView.addItemDecoration(ItemOffsetDecoration(14))
+
+
+
+        getData(model?.id)
+
+
+    }
+
+    fun configureRows(list: List<AlbumModel>) {
+        adapter.clear()
+        for (i in list) {
+            adapter.add(AlbumComponent(i))
+        }
+    }
+
+    fun getData(id: String?) {
+adapter.isLoading=true
+        var call = RetrofitClient.retrofitInterface(context).getArtistAlbums(id)
+
+
+        call.enqueue(
+            RetrofitCallback(
+                this@ArtistDetailFragment.requireContext(),
+                object : Callback<ArrayAlbumModel?> {
+
+                    override fun onResponse(
+                        call: Call<ArrayAlbumModel?>,
+                        response: Response<ArrayAlbumModel?>
+                    ) {
+                        if (response.code() == 200) {
+                            adapter.isLoading=false
+                            configureRows(response.body()?.data as List<AlbumModel>)
+                        }
+                    }
+
+                    override fun onFailure(
+                        call: Call<ArrayAlbumModel?>,
+                        t: Throwable
+                    ) {
+                        Toast.makeText(
+                            this@ArtistDetailFragment.requireContext(),
+                            t.message,
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+
+                })
+        )
+    }
+
+}

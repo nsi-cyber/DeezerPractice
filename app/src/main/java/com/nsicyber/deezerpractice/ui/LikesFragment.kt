a1a1dabbafd3e980292ce5dca4b0d449252afee8
Enes <enes.ay@dijitalsahne.com>
2023-05-12 11:55:19 +0300

    Likes page implementation

--- app/src/main/java/com/nsicyber/deezerpractice/ui/LikesFragment.kt
+++ app/src/main/java/com/nsicyber/deezerpractice/ui/LikesFragment.kt
@@ -5,11 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ace1ofspades.recyclerview.GroupAdapter
+import com.ace1ofspades.recyclerview.viewHolders.ViewHolder
+import com.nsicyber.deezerpractice.PreferencesHelper
 import com.nsicyber.deezerpractice.R
-
+import com.nsicyber.deezerpractice.components.MusicComponent
+import com.nsicyber.deezerpractice.components.SearchListComponent
+import com.nsicyber.deezerpractice.models.AlbumModel
+import com.nsicyber.deezerpractice.models.MusicModel
 
 
 class LikesFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+    var adapter = GroupAdapter<ViewHolder>()
 
 
     override fun onCreateView(
@@ -17,7 +27,38 @@ class LikesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_likes, container, false)
+        var view = inflater.inflate(R.layout.fragment_likes, container, false)
+
+        recyclerView = view.findViewById(R.id.recyclerView)
+
+        return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView.layoutManager =
+            LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        recyclerView.adapter = adapter
+        configureRows()
+
+    }
+
+
+    private fun configureRows(
+    ) {
+        var list = PreferencesHelper(requireContext()).likedMusics as ArrayList<MusicModel>
+        adapter.clear()
+        adapter.add(SearchListComponent(list).apply {
+            this.fragment = this@LikesFragment
+        })
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        configureRows()
+    }
+
 
 }

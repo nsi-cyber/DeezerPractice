ebef26f84f3374edc5c4d45113e67636e8f7fd9a
Enes <enes.ay@dijitalsahne.com>
2023-05-12 11:55:47 +0300

    ViewPager2 and tablayout implementation

--- app/src/main/java/com/nsicyber/deezerpractice/MainActivity.kt
+++ app/src/main/java/com/nsicyber/deezerpractice/MainActivity.kt
@@ -2,8 +2,13 @@ package com.nsicyber.deezerpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import com.nsicyber.deezerpractice.databinding.ActivityMainBinding
+import com.nsicyber.deezerpractice.ui.LikesFragment
+import com.nsicyber.deezerpractice.ui.MainFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,9 +19,31 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val adapter = MyPagerAdapter(this)
+        binding.viewPager.adapter = adapter
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "Kategoriler"
+                1 -> tab.text = "Beğeniler"
+            }
+        }.attach()
+    }
 
 
     }
 
 
+
+
+class MyPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> MainFragment()
+            1 -> LikesFragment()
+            else -> throw IndexOutOfBoundsException()
+        }
+    }
 }
